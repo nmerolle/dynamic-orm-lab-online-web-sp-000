65e2d1ad481fab67731798c269c28f99e7a7bd5c
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-08 02:56:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,5 +59,11 @@ class InteractiveRecord
   def self.find_by_name(name)
   DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
   end
-    
+  
+  def self.find_by(hash)
+    key, value = hash.first
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key.to_s} = ?"
+    DB[:conn].execute(sql, value)
+  end
+  
 end
